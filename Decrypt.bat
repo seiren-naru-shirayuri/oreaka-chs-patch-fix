066df4b967e1f65de8a14d4a73e47d474c7e1d79
seiren-naru-shirayuri <56169262+seiren-naru-shirayuri@users.noreply.github.com>
2020-07-30 21:06:39 +0800

    Add files via upload

--- Decrypt.bat
+++ Decrypt.bat
@@ -4,9 +4,9 @@ copy /b opmv.wmv.000 + opmv.wmv.001 + opmv.wmv.002 + opmv.wmv.003 + opmv.wmv.004
 del opmv.wmv.???
 ren opmv.wmv.merged opmv.wmv
 cd ..
-md "V1.3 Patch"
-move patch3_encrypted\evz01_10.png "V1.3 Patch\patch3.xp3"
 for %%i in (patch3_encrypted\*) do cscript //nologo XorCrypt.vbs 123 %%i %%i.xored
+md "V1.3 Patch"
+move patch3_encrypted\evz01_10.png.xored "V1.3 Patch\patch3.xp3"
 md patch3
 move patch3_encrypted\*.xored patch3\
 cd patch3
